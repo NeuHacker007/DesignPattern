80facb151045ec1d02bbb707514a962a77da11ce
Yifan Zhang <yf.eva.yifan@gmail.com>
2019-05-16 20:17:15 -0400

    this is V3 version we use constructor injection

--- JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/dependencyinversion/Ivan.java
+++ JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/dependencyinversion/Ivan.java
@@ -1,13 +1,15 @@
 package com.ivan.design.principle.dependencyinversion;
 
 public class Ivan {
-    /*
-    * Instead of pass in a concrete implementation class, we pass an Interface. So, in this case, we
-    * can pass any concrete class which implemented ICourse interface. And, we are not care about the
-    * details of which class to study.
-    * This injection is method injection, we still can inject to constructor as well
-    */
-    public void StudySDECourse(ICourse iCourse){
-        iCourse.StudyCourse();
+
+    private ICourse iCourse;
+
+    public Ivan(ICourse iCourse){
+
+        this.iCourse = iCourse;
+    }
+
+    public void StudySDECourse(){
+        this.iCourse.StudyCourse();
     }
 }
--- JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/dependencyinversion/Test.java
+++ JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/dependencyinversion/Test.java
@@ -2,16 +2,17 @@ package com.ivan.design.principle.dependencyinversion;
 
 public class Test {
     public static void main(String[] args) {
+        //V2
+        //Ivan ivan = new Ivan();
+        //ivan.StudySDECourse(new JavaCourse());
+        //ivan.StudySDECourse(new PythonCourse());
+        //ivan.StudySDECourse(new FECourse());
         /*
-        * In this case, if we want to add new course, we don't need to modify the ivan class, the ICourse interface, and
-        * the concrete course implementation class. The only thing we should to do is to add a new class for new course
-        * which implements the ICourse interface. For example, we current want to add a FE course then, we add new class
-        * of FECourse and call from here
+        * V3 the constructor injection has limitation, if now I want to study Java Course
+        * I have to initial a new instance of ivan to make it work
         */
-        Ivan ivan = new Ivan();
-        ivan.StudySDECourse(new JavaCourse());
-        ivan.StudySDECourse(new PythonCourse());
-        ivan.StudySDECourse(new FECourse());
+        Ivan ivan = new Ivan(new PythonCourse());
+        ivan.StudySDECourse();
 
     }
 }
