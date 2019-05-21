2d649755888b84a662544c697a3ce48744dd1998
Yifan Zhang <yf.eva.yifan@gmail.com>
2019-05-20 22:38:49 -0400

    We break down the ICourse into ICourseManager and ICourseContent

---
+++ JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/singleresponsibility/ICourseContent.java
@@ -0,0 +1,6 @@
+package com.ivan.design.principle.singleresponsibility;
+
+public interface ICourseContent {
+    String getCourseName(Integer Id);
+    byte[] getCourseVideo();
+}
---
+++ JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/singleresponsibility/ICourseManager.java
@@ -0,0 +1,6 @@
+package com.ivan.design.principle.singleresponsibility;
+
+public interface ICourseManager {
+    void registerCourse(Integer Id);
+    void unRegisterCourse(Integer Id);
+}
