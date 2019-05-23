052a1ba56579280237823500932e5dc4c7fd3c50
Yifan Zhang <yf.eva.yifan@gmail.com>
2019-05-22 22:22:44 -0400

    this version the Boss class reduce the dependency of Boss and course class

--- JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/demter/TeamLeader.java
+++ JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/principle/demter/TeamLeader.java
@@ -1,9 +1,15 @@
 package com.ivan.design.principle.demter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TeamLeader {
-    void getCourseNumber(List<Course> courses){
+    void getCourseNumber(){
+        List<Course> courses = new ArrayList<Course>();
+
+        for (int i=0;i<20;i++) {
+            courses.add(new Course());
+        }
         System.out.println("The total number of courses is: " + courses.size());
     }
 }
