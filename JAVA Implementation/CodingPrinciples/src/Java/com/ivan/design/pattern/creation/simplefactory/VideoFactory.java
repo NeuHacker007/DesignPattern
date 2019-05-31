e3d362764aad66a3b7be7117ff830fee4db32d83
Yifan Zhang <yf.eva.yifan@gmail.com>
2019-05-30 22:36:17 -0400

    Improved the simple factory to just passing the class name and it will generate corresponding object and returned

--- JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/pattern/creation/simplefactory/VideoFactory.java
+++ JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/pattern/creation/simplefactory/VideoFactory.java
@@ -1,12 +1,29 @@
 package com.ivan.design.pattern.creation.simplefactory;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class VideoFactory {
-    public Video getVideo(String type){
-        if ("java".equalsIgnoreCase(type)) {
-            return new JavaVideo();
-        } else if ("python".equalsIgnoreCase(type)) {
-            return new PythonVideo();
+    public Video getVideo(Class c){
+        Video video = null;
+        try {
+           video  = (Video) Class.forName(c.getName()).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
-        return null;
+        return video;
+//        if ("java".equalsIgnoreCase(type)) {
+//            return new JavaVideo();
+//        } else if ("python".equalsIgnoreCase(type)) {
+//            return new PythonVideo();
+//        }
+//        return null;
     }
 }
