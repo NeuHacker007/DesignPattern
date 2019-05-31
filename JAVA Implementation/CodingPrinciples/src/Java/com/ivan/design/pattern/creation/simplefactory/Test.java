177b6c220750e27e18b2d8b7bee4019eb745f98f
Yifan Zhang <yf.eva.yifan@gmail.com>
2019-05-30 22:38:26 -0400

    Improved the simple factory to just passing the class name and it will generate corresponding object and returned. this modification makes the implementation follow open close principle because when we add new class, we don't need to modify the factory class any more.

--- JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/pattern/creation/simplefactory/Test.java
+++ JAVA Implementation/CodingPrinciples/src/Java/com/ivan/design/pattern/creation/simplefactory/Test.java
@@ -2,15 +2,22 @@ package com.ivan.design.pattern.creation.simplefactory;
 
 public class Test {
     public static void main(String[] args) {
+//
+//        VideoFactory videoFactory = new VideoFactory();
+//        Video video = videoFactory.getVideo("python");
+//        if (video == null) {
+//            return;
+//        }
+//        video.produce();
+
+//        Video video = new JavaVideo();
+//        video.produce();
 
         VideoFactory videoFactory = new VideoFactory();
-        Video video = videoFactory.getVideo("python");
+        Video video = videoFactory.getVideo(PythonVideo.class);
         if (video == null) {
             return;
         }
         video.produce();
-
-//        Video video = new JavaVideo();
-//        video.produce();
     }
 }
