215f496fb545b83638c5c0a2305045de5209a0b4
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-08-18 13:24:28 -0400

    --Initialize the Authenticator Demo

---
+++ DotNetImplementation/GeekTime/AuthenticatorDemo/AuthenticatorDemo/AuthenticatorDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthenticatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
